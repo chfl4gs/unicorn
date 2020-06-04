564c78648f1a43d823b2074a324b50bfbcd313c5
chfl4gs <chbsd64@gmail.com>
2020-06-05 00:51:23 +0800

    dockcross

---
+++ .github/workflows/build-wheel.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e -x
+
+cd binding/python
+
+# Compile wheels
+for PYBIN in /opt/python/*/bin; do
+    ${PYBIN}/pip wheel . -w wheelhouse/
+done
+
+# Bundle external shared libraries into the wheels
+for whl in wheelhouse/*.whl; do
+    auditwheel repair $whl -w ./wheelhouse/
+done
+
+# Install packages and test
+for PYBIN in /opt/python/*/bin/; do
+    sudo ${PYBIN}/pip install unicorn --no-index -f ./wheelhouse
+done
