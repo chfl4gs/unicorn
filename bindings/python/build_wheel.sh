e736b008e542f04f11f64fb99efe04782779f2ce
chfl4gs <chbsd64@gmail.com>
2020-06-06 01:36:01 +0800

    PyPI distribution packages workflow pypa manylinux1

--- bindings/python/build_wheel.sh
+++ bindings/python/build_wheel.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
-set -e -x
+set -e -u -x
 
-cd bindings/python
+function repair_wheel {
+    wheel="$1"
+    if ! auditwheel show "$wheel"; then
+        echo "Skipping non-platform wheel $wheel"
+    else
+        auditwheel repair "$wheel" --plat "$PLAT" -w /work/bindings/python/dist/
+    fi
+}
+
 
 # Compile wheels
-if [ -f /opt/python/cp36-cp36m/bin/python ];then
-  /opt/python/cp36-cp36m/bin/python setup.py bdist_wheel
-else
-  python3 setup.py bdist_wheel
-fi
-cd dist
-auditwheel repair *.whl
-mv -f wheelhouse/*.whl .
+for PYBIN in /opt/python/*/bin; do
+    "${PYBIN}/pip" wheel /work/bindings/python/ --no-deps -w wheelhouse/
+done
+
+# Bundle external shared libraries into the wheels
+for whl in wheelhouse/*.whl; do
+    repair_wheel "$whl"
+done
+
+# Install packages and test
+for PYBIN in /opt/python/*/bin/; do
+    "${PYBIN}/pip" install unicorn --no-index -f /work/bindings/python/dist
+    (cd "$HOME"; "${PYBIN}/nosetests" unicorn)
+done
