f67f1b514f604bd364700a7906298a6bf1d59738
chfl4gs <chbsd64@gmail.com>
2020-06-06 03:04:56 +0800

    PyPI distribution packages workflow pypa manylinux1

--- bindings/python/build_wheel.sh
+++ bindings/python/build_wheel.sh
@@ -23,8 +23,7 @@ for whl in wheelhouse/*.whl; do
     repair_wheel "$whl"
 done
 
-# Install packages and test
+# Install packages
 for PYBIN in /opt/python/*/bin/; do
     "${PYBIN}/pip" install unicorn --no-index -f /work/bindings/python/dist
-    (cd "$HOME"; "${PYBIN}/nosetests" unicorn)
 done
