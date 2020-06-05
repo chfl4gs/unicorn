5f6c2c0bc4bf9fdcb6a6f80e8523b80ff7ecaa65
chfl4gs <chbsd64@gmail.com>
2020-06-05 18:06:01 +0800

    dockcross distribution build

--- bindings/python/build_wheel.sh
+++ bindings/python/build_wheel.sh
@@ -7,12 +7,9 @@ cd bindings/python
 if [ -f dist/unicorn-*.tar.gz ]; then
    /opt/python/cp36-cp36m/bin/python setup.py sdist
 fi
-/opt/python/cp36-cp36m/bin/python setup.py bdist_wheel
-
-cd dist
-for i in *.whl
-  do
-    auditwheel repair $i
-done
-mv -f wheelhouse/*.whl .
+if [ -f /opt/python/cp36-cp36m/bin/python ];then
+  /opt/python/cp36-cp36m/bin/python setup.py bdist_wheel
+else
+  python setup.py bdist_wheel
+fi
 cd /work && make clean
